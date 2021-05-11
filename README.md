9226d042ebde917730413ba96c13f3cb97d1df74
dashanji <952129620@qq.com>
2021-05-11 23:00:48 +0800

    add test

--- README.md
+++ README.md
@@ -1,3 +1,19 @@
 # docker_in_go
-A simple implementation of docker in Golang
+A simple implementation of docker in Golang.
 The project is mostly based on https://github.com/pibigstar/go-docker
+How to run ?
+CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build ./main.go
+After this command,you need to move <main> to Linux
+1、Test hostname
+![img.png](img.png)
+2、Test PID
+![img_2.png](img_2.png)
+3、Test Mount
+![img_4.png](img_4.png)
+![img_5.png](img_5.png)
+4、Test User
+![img_6.png](img_6.png)
+5、Test Network
+![img_3.png](img_3.png)
+6、Test IPC
+![img_1.png](img_1.png)
--- main.go
+++ main.go
@@ -1,18 +1,48 @@
-package docker_in_go
+package main
 
 import (
-"log"
-"os"
-"os/exec"
-"syscall"
+	"log"
+	"os"
+	"os/exec"
+	"syscall"
 )
 func main(){
 	cmd := exec.Command("sh")
 
 	cmd.SysProcAttr = &syscall.SysProcAttr{
-		//uts: 用来隔离主机名
-		Cloneflags: syscall.CLONE_NEWUTS,
-		//ipc：用来隔离 System V IPC 和 POSIX message queues
+		            //uts: 用来隔离主机名
+		Cloneflags: syscall.CLONE_NEWUTS |
+					//pid:用来隔离进程 PID 号
+					syscall.CLONE_NEWPID |
+					//mount:用来隔离各个进程看到的挂载点视图
+					syscall.CLONE_NEWNS  |
+					//user:用来隔离用户
+					syscall.CLONE_NEWUSER|
+					//network:用来隔离网络
+					syscall.CLONE_NEWNET |
+					//ipc:用来隔离 System V IPC 和 POSIX message queues
+					syscall.CLONE_NEWIPC,
+		//设置容器的UID
+		UidMappings: []syscall.SysProcIDMap{
+			{
+				//容器UID
+				ContainerID: 1,
+				//宿主机UID
+				HostID: 0,
+				Size: 1,
+			},
+		},
+		//设置容器的GID
+		GidMappings: []syscall.SysProcIDMap{
+			{
+				//容器GID
+				ContainerID: 1,
+				//宿主机GID
+				HostID: 0,
+				Size: 1,
+			},
+		},
+
 	}
 	cmd.Stdin = os.Stdin
 	cmd.Stdout = os.Stdout
